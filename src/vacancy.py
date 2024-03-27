9780e3f9b4bc70cf83b6c74ab1e5fef245359b58
konov <konovalova_ekaterina_a@mail.ru>
2024-03-27 23:27:24 +0300

    add more methods

--- src/vacancy.py
+++ src/vacancy.py
@@ -7,3 +7,23 @@ class Vacancy:
         self.link = link
         self.salary = salary
         self.description = description
+
+        if not isinstance(self.salary, (str, int, float)):
+            self.salary = "Зарплата не указана"
+
+    def __str__(self):
+        return f"{self.title}\nЗарплата: {self.salary}\nСсылка: {self.link}\nОписание: {self.description}\n"
+
+    def __lt__(self, other):
+        return self.salary < other.salary
+
+    def to_json(self):
+        return json.dumps(self.__dict__)
+
+    @staticmethod
+    def from_json(json_str):
+        data = json.loads(json_str)
+        return Vacancy(data['title'], data['link'], data['salary'], data['description'])
+
+
+
