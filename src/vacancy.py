e6476c5ef99f1ef5d1a38b4738b395b2bb204224
konov <konovalova_ekaterina_a@mail.ru>
2024-03-27 22:40:18 +0300

    add new class Vacancy

---
+++ src/vacancy.py
@@ -0,0 +1,9 @@
+import json
+
+
+class Vacancy:
+    def __init__(self, title, link, salary, description):
+        self.title = title
+        self.link = link
+        self.salary = salary
+        self.description = description
